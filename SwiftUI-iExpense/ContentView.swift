b21f641c031fdac831a2e0b07e4340d37fc869c6
Jimmy <myname90196@gmail.com>
2024-03-23 11:40:49 +0800

    Initial Commit

---
+++ SwiftUI-iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-iExpense
+//
+//  Created by JimmyChao on 2024/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-iExpense/SwiftUI_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iExpenseApp.swift
+//  SwiftUI-iExpense
+//
+//  Created by JimmyChao on 2024/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
